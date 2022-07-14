076706a33116e25032a781336fcebe1383129a28
romitkarmakar <romitkarmakar@gmail.com>
2022-07-15 04:03:04 +0530

    Added webshare api

--- components/TweetCard.tsx
+++ components/TweetCard.tsx
@@ -11,6 +11,25 @@ interface IProps {
 }
 
 export default function TweetCard({ tweet, animate }: IProps) {
+  const shareTweet = async () => {
+    if (navigator.share) {
+      try {
+        await navigator
+          .share({
+            url: tweet.url,
+            text: tweet.text,
+          })
+          .then(() => console.log("Successfully shared the tweet"));
+      } catch (error) {
+        console.log(`Failed to share tweet: ${error}`);
+      }
+    } else {
+      console.log(
+        "Web share is currently not supported on this browser."
+      );
+    }
+  };
+
   return (
     <AnimationOnScroll animateIn="animate__fadeIn" initiallyVisible={!animate}>
       <div className="shadow-lg rounded-lg p-8 my-2 hover:bg-gray-100 max-w-2xl">
@@ -70,23 +89,25 @@ export default function TweetCard({ tweet, animate }: IProps) {
                 <span className="ml-1 text-sm">{tweet.retweets}</span>
               </div>
               <div className="flex-grow" />
-              <div className="inline-flex items-center text-gray-500 hover:text-blue-500 cursor-pointer">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z"
-                  />
-                </svg>
-                <span className="ml-1 text-sm">Share</span>
-              </div>
+              <button onClick={shareTweet}>
+                <div className="inline-flex items-center text-gray-500 hover:text-blue-500">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z"
+                    />
+                  </svg>
+                  <span className="ml-1 text-sm">Share</span>
+                </div>
+              </button>
             </div>
           </div>
         </div>
